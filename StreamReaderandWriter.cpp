7be0d06576ae54dc3093436711f5755998daf21c
khansaasasabilaaa-eng <khansaasasabilaaa@gmail.com>
2026-06-02 18:07:00 +0700

    deklarasi variabel baris

---
+++ StreamReaderandWriter.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+    //deklarasi variable baris untuk menyimpan data string
+    string baris;
+
+    //membuka file dalam mode menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file
+    outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl; 
+
+    
+}
