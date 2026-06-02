01c56e05694eeb58e366636d7f39fe4d62dbd9ec
khansaasasabilaaa-eng <khansaasasabilaaa@gmail.com>
2026-06-02 17:33:44 +0700

    menambahkan panjang array

--- ExceptionStandart.cpp
+++ ExceptionStandart.cpp
@@ -2,3 +2,16 @@
 #include <exception>
 #include <array>
 using namespace std;
+
+
+int main()
+{
+
+    cout << "Awal program" << endl; //penanda 1
+    try
+    {
+        //deklarasi array data dengan panjang index 3
+        array <int, 3> data = { 1, 2, 3 };
+        
+    
+}
